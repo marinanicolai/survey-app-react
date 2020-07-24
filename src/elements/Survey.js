2aa56b199be65f844029044022f6371adc7319b2
Marina Nicolai <nicolaimarina@gmail.com>
2020-07-24 12:28:24 -0400

    installing the firebase

--- src/elements/Survey.js
+++ src/elements/Survey.js
@@ -1,6 +1,22 @@
 import React from "react";
-
+import firebase from "firebase";
 import { StyledSurvey } from "../styles/StyledSurvey";
+
+// var uuid = require("uuid");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC70PIByKfKt-uwFOtVhIwENaYBzswhkQs",
+  authDomain: "uservey-f06ad.firebaseapp.com",
+  databaseURL: "https://uservey-f06ad.firebaseio.com",
+  projectId: "uservey-f06ad",
+  storageBucket: "uservey-f06ad.appspot.com",
+  messagingSenderId: "409322652639",
+  appId: "1:409322652639:web:9a35073617046e86263a6a",
+  measurementId: "G-M6ZC9KJB3H",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 const Survey = () => (
   <StyledSurvey>
